5b81d71eb940cbfaf10674a8d784c9828e5e6099
raojay7 <879645920@qq.com>
2018-03-13 15:08:03 +0800

    更新页面及ajax

--- src/main/java/com/hlzj/test/controller/TestController.java
+++ src/main/java/com/hlzj/test/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.hlzj.test.controller;
 
+import com.alibaba.druid.sql.visitor.functions.If;
+import com.hlzj.test.entity.CareResult;
 import com.hlzj.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController
@@ -18,4 +22,23 @@ public class TestController
         testService.test();
         return "index";
     }
+
+    @RequestMapping("bed_control")
+    @ResponseBody
+    public CareResult test(String instruction){
+        CareResult result=new CareResult();
+        result.setStatus(200);
+        if(instruction.isEmpty()){
+            result.setStatus(400);
+            return result;
+        }
+        System.out.println(instruction);
+        return result;
+    }
+
+    @RequestMapping("/{path}")
+    public String showPath(@PathVariable  String path){
+        return path;
+    }
+
 }
